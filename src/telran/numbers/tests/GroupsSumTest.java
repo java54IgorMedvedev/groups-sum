f90e3055bed88ce4f3e06dacc115d5baed07bd7f
IgorMedvedev <psydov81@gmail.com>
2024-08-05 18:08:33 +0300

    cw

--- src/telran/numbers/tests/GroupsSumTest.java
+++ src/telran/numbers/tests/GroupsSumTest.java
@@ -1,47 +1,43 @@
 package telran.numbers.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+
 import java.util.Random;
 import java.util.stream.Stream;
-import telran.numbers.*;
 
+import org.junit.jupiter.api.Test;
+import telran.numbers.*;
 class GroupsSumTest {
-    private static final int N_GROUPS = 1000;
-    private static final int GROUP_LENGTH = 1000;
-    int[][] groups = {
-        {1,2},
-        {3,4},
-        {5,6}
-    };
-    int[][] largeGroups = getLargeGroups(N_GROUPS, GROUP_LENGTH);
-    
-    @Test
-    void goupsSumTaskThreadTest() {
-        GroupsSum gs = new GroupsSumTaskThread(groups);
-        assertEquals(21, gs.getSum());
-    }
-    
-    private int[][] getLargeGroups(int nGroups, int groupLength) {
-        Random random = new Random();
-        return Stream.generate(() -> random.ints(groupLength, 1, 100).toArray())
-                .limit(nGroups)
-                .toArray(int[][]::new);
-    }
-    
-    @Test
-    void goupsSumThreadPoolTest() {
-        GroupsSum gs = new GroupsSumThreadPool(groups);
-        assertEquals(21, gs.getSum());
-    }
-    
-    @Test
-    void groupsSumTaskThreadPerformance() {
-        new GroupsSumTaskThread(largeGroups).getSum();
-    }
-    
-    @Test
-    void groupsSumTaskThreadPoolsPerformance() {
-        new GroupsSumThreadPool(largeGroups).getSum();
-    }
+private static final int N_GROUPS = 1000000;
+private static final int GROUP_LENGTH = 100;
+int[][] groups = {
+		{1,2},
+		{3,4},
+		{5,6}
+};
+int[][] largeGroups = getLargeGroups(N_GROUPS, GROUP_LENGTH);
+	@Test
+	void goupsSumTaskThreadTest() {
+		GroupsSum gs = new GroupsSumTaskThread(groups);
+		assertEquals(21, gs.getSum());
+	}
+	private int[][] getLargeGroups(int nGroups, int groupLength) {
+		
+		return Stream.generate(() -> new Random().ints(groupLength).toArray())
+				.limit(nGroups).toArray(int[][]::new);
+	}
+	@Test
+	void goupsSumThreadPoolTest() {
+		GroupsSum gs = new GroupsSumThreadPool(groups);
+		assertEquals(21, gs.getSum());
+	}
+	@Test
+	void groupsSumTaskThreadPerformance() {
+		new GroupsSumTaskThread(largeGroups).getSum();
+	}
+	@Test
+	void groupsSumTaskThreadPoolsPerformance() {
+		new GroupsSumThreadPool(largeGroups).getSum();
+	}
+
 }
